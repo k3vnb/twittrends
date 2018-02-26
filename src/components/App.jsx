3cdae93904afef570c156cafffd820bac2a7afc8
Kevin Boyle and Mikey Pinaud <mikey@>
2018-02-26 15:17:14 -0800

    set up page w/ dependencies

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      Twends
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import thunkMiddleware from 'redux-thunk';
+// import middlewareLogger from './middleware/middleware-logger';
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('react-app-root')
+);
